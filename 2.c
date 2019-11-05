1a413f8ec1eb32f2c339c3e8955e4cc0b25f45c5
Seonghun Kim <thunder9510@naver.com>
2019-11-05 23:05:34 +0900

    mynameis2
    2222

--- 2.c
+++ 2.c
@@ -1,1 +1,2 @@
-#include “myProject.h" int printMyName2(void) //1~4중 하나. 파일이름에 맞게 할 것 { printf (“my name is[김성훈]\n”); //자기 이름 return 1; }
+#include “myProject.h" int printMyName2(void) //1~4중 하나. 파일이름에 맞게 할 것 
+{ printf (“my name is[김성훈]\n”); //자기 이름 return 1; }
