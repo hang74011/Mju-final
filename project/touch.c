680ad94e51d7653c230e37c02b5b34b5ea1fdaab
SeokHee Hong <hang7401@mju.ac.kr>
2019-12-12 19:31:16 +0900

    touch

--- project/touch.c
+++ project/touch.c
@@ -5,6 +5,7 @@
 #include <sys/ioctl.h>
 #include <linux/input.h>
 #include <pthread.h>
+#include <sys/msg.h>
 #include "touch.h"
 
 #define EVENT_DEVICE    "/dev/input/event4"
@@ -12,23 +13,17 @@
 #define EVENT_CODE_X    ABS_X
 #define EVENT_CODE_Y    ABS_Y
 
+pthread_t touchTh_id;
 int fd;
 int a,b,c,x,y = 0;
+static int msgID;
 
-int touchFunc(void)
+void* touchThFunc(void *args)
 {
-	 struct input_event ev;
-    char name[256] = "Unknown";
-
-    /* Print Device Name */
-    ioctl(fd, EVIOCGNAME(sizeof(name)), name);
-    printf("Reading from:\n");
-    printf("device file = %s\n", EVENT_DEVICE);
-    printf("device name = %s\n", name);
-    
-    for (;;) 
-	    {
-			const size_t ev_size = sizeof(struct input_event);
+	struct input_event ev;
+	while(1)
+	{
+		const size_t ev_size = sizeof(struct input_event);
 			ssize_t size;
 			
 			size = read(fd, &ev, ev_size);
@@ -51,16 +46,33 @@ int touchFunc(void)
 					y = ev.value;
 					b = 10;
 				}
-				//printf("%s = %d\n", ev.code == EVENT_CODE_X ? "X" : "Y", ev.value);
-				if(a+b == 20)
-				{
-					printf("x좌표 = %d	y좌표 = %d\n", x, y);
-					a = 0;
-					b = 0;
-				}
 			}
+		if(a+b == 20)
+		{
+			 TOUCH_MSG_T msgTx;
+			msgTx.messageNum = 1;
+			//printf("x좌표 = %d	y좌표 = %d\n", x, y);
+			msgTx.touchX = x;
+			msgTx.touchY = y;
+			msgsnd(msgID, &msgTx, sizeof(msgTx)-sizeof(long int), 0);
+			a = 0;
+			b = 0;
 		}
+	}
 	err:
+	return 0;
+}
+
+int touchFunc(void)
+{
+    char name[256] = "Unknown";
+
+    /* Print Device Name */
+    ioctl(fd, EVIOCGNAME(sizeof(name)), name);
+    printf("Reading from:\n");
+    printf("device file = %s\n", EVENT_DEVICE);
+    printf("device name = %s\n", name);
+    
 	return 0;
 }
 
@@ -70,15 +82,17 @@ int touchInit(void)		// Open Device
 		fprintf(stderr, "You are not root! This may not work...\n");
         return EXIT_SUCCESS;
     }
-	
 	fd = open(EVENT_DEVICE, O_RDONLY);
 	if (fd == -1) {
 		fprintf(stderr, "%s is not a vaild device\n", EVENT_DEVICE);
 		return EXIT_FAILURE;
     }
+    msgID = msgget(MESSAGE_ID, IPC_CREAT|0666); //1234
+    pthread_create(&touchTh_id, NULL, &touchThFunc, NULL);
 }
 
 int touchExit(void)		
 {
+	pthread_cancel(touchTh_id);
 	close(fd);
 }
