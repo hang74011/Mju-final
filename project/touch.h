938641e0d396f5ed53a2853554575f65b33c5f8c
SeokHee Hong <hang7401@mju.ac.kr>
2019-12-12 19:31:22 +0900

    touch

--- project/touch.h
+++ project/touch.h
@@ -1,6 +1,16 @@
 #ifndef _TOUCH_H_
 #define _TOUCH_H_
 
+#define MESSAGE_ID 1234
+
+typedef struct
+{
+	long int messageNum;
+	int touchX;
+	int touchY;
+} TOUCH_MSG_T;
+
+
 int touchInit(void);
 int touchFunc(void);
 int touchExit(void);
