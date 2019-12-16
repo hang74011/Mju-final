e29f1bf5af725a64e8e29a2e11a08a80f518423d
songgilho <wmdmfaksen@mju.ac.kr>
2019-12-16 18:27:05 +0900

    gdgd
    gdgd

--- project/buttontest.c
+++ project/buttontest.c
@@ -1,26 +1,26 @@
-#include <string.h>
-#include <linux/input.h>
-#include <unistd.h> 
-#include <fcntl.h> 
-#include <sys/ioctl.h> 
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-#include <sys/ipc.h>
-#include <sys/msg.h>
-#define  INPUT_DEVICE_LIST	"/dev/input/event"
-#define  SYS_PATH	"S: Sysfs="
-#define  BUTTON_NAME	"ecube-button"
-#define  LINE_FEED	0x0A
-#define  MAX_BUFF	200
-int main(int argc, char *argv[])
+#include <string.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <linux/input.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <unistd.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <fcntl.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <sys/ioctl.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <sys/mman.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <fcntl.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <unistd.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <stdio.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <stdlib.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <pthread.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <sys/ipc.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#include <sys/msg.h> // 프로그램을 실행시키기 위해 쓰는 함수들을 위해 헤더파일 추가
+#define  INPUT_DEVICE_LIST	"/dev/input/event" //가져다 쉽게 불러쓰기 위해 define 함수 씀
+#define  SYS_PATH	"S: Sysfs=" //가져다 쉽게 불러쓰기 위해 define 함수 씀
+#define  BUTTON_NAME	"ecube-button" //가져다 쉽게 불러쓰기 위해 define 함수 씀
+#define  LINE_FEED	0x0A //가져다 쉽게 불러쓰기 위해 define 함수 씀
+#define  MAX_BUFF	200 //가져다 쉽게 불러쓰기 위해 define 함수 씀
+int main(int argc, char *argv[]) //함수 배열 선언
 {
 
-	char 	inputfileName[MAX_BUFF+1];
-	int		readSize,inputIndex;
+	char 	inputfileName[MAX_BUFF+1]; // 변수 선언
+	int		readSize,inputIndex; // 변수 선언
 	struct input_event  stEvent;
 	if (argc != 2)
 	{
@@ -30,7 +30,7 @@ int main(int argc, char *argv[])
 		
 		return 1;
 	}
-	inputIndex = atoi(argv[1]);
+	inputIndex = atoi(argv[1]); // 글자를 잘라서 저장하기 위해 씀
 
 	sprintf(inputfileName,"%s%d",INPUT_DEVICE_LIST,inputIndex);
 	printf("read button event:%s\n",inputfileName);
